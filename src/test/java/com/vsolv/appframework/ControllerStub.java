fceefc8f2d9f1b81d68d60f9722c01ad72b86eff
philowest <philowest@gmail.com>
2017-02-03 14:24:41 +1100

    sample test

---
+++ src/test/java/com/vsolv/appframework/ControllerStub.java
@@ -0,0 +1,12 @@
+package com.vsolv.appframework;
+
+class ControllerStub {
+
+    public void methodWithAnnotation(@GetJsonRequestParam Object param) {
+
+    }
+
+    public void methodWithoutAnnotation(Object param) {
+
+    }
+}
---
+++ src/test/java/com/vsolv/appframework/GetJsonArgumentResolverTest.java
@@ -0,0 +1,41 @@
+package com.vsolv.appframework;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.MethodParameter;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GetJsonArgumentResolverTest {
+
+    @Test
+    @DisplayName("Test method with an annotation")
+    public void supportsMethodWithAnnotationTest() {
+        GetJsonArgumentResolver resolver = new GetJsonArgumentResolver();
+        try {
+            Method method = ControllerStub.class.getMethod("methodWithAnnotation", Object.class);
+            MethodParameter methodParameter = new MethodParameter(method, 0);
+            assertTrue(resolver.supportsParameter(methodParameter));
+        } catch (NoSuchMethodException e) {
+            fail("No such method in the test class");
+        }
+    }
+
+    @Test
+    @DisplayName("Test method without an annotation")
+    public void supportsMethodWithoutAnnotationTest() {
+        GetJsonArgumentResolver resolver = new GetJsonArgumentResolver();
+        try {
+            Method method = ControllerStub.class.getMethod("methodWithoutAnnotation", Object.class);
+            MethodParameter methodParameter = new MethodParameter(method, 0);
+            assertFalse(resolver.supportsParameter(methodParameter));
+        } catch (NoSuchMethodException e) {
+            fail("No such method in the test class");
+        }
+    }
+
+}
