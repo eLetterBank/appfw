82ac2e6a6f44d2a83e0219d99e8af9d9d8d44558
Phil West <philowest@users.noreply.github.com>
2017-02-03 14:52:33 +1100

    Unit tests (#2)
    * sample test
    
    * Change unit test package

---
+++ src/test/java/com/vsolv/appframework/http/request/ControllerStub.java
@@ -0,0 +1,12 @@
+package com.vsolv.appframework.http.request;
+
+class ControllerStub {
+
+    public void methodWithAnnotation(@GetJsonRequestParam Object param) {
+
+    }
+
+    public void methodWithoutAnnotation(Object param) {
+
+    }
+}
